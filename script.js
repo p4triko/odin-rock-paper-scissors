3d2c5710d53f8b5e8508539169aadb1100d9084d
p4triko <168976850+p4triko@users.noreply.github.com>
2024-05-15 17:34:06 +0300

    Create function to get the human input for the game. Edit page title

--- script.js
+++ script.js
@@ -2,9 +2,28 @@ const ROCK = 1;
 const PAPER = 2;
 const SCISSORS = 3;
 
-function getComputerChoice(min, max) {
-  let result = Math.floor(Math.random() * max) + min;
-  return result;
+function getHumanChoice() {
+  for (;;) {
+    let playerChoice = prompt("Choose Rock, Paper or Scissors: ").toUpperCase();
+    if (playerChoice === "ROCK") {
+      playerChoice = ROCK;
+      return playerChoice;
+    } else if (playerChoice === "PAPER") {
+      playerChoice = PAPER;
+      return playerChoice;
+    } else if (playerChoice === "SCISSORS") {
+      playerChoice = SCISSORS;
+      return playerChoice;
+    } else {
+      alert('Please enter valid input "Rock" "Paper" or "Scissors"');
+    }
+  }
 }
 
-console.log(getComputerChoice(ROCK, SCISSORS));
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * SCISSORS) + ROCK;
+  return computerChoice;
+}
+
+console.log(getHumanChoice());
+console.log(getComputerChoice());
