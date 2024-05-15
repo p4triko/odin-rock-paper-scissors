441fa07aa01356a6a0de179b420523245578ec15
p4triko <168976850+p4triko@users.noreply.github.com>
2024-05-15 20:14:58 +0300

    Add game states / extra info for the player

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const ROCK = 1;
 const PAPER = 2;
 const SCISSORS = 3;
 
+const ROCK_STRING = "Rock";
+const PAPER_STRING = "Paper";
+const SCISSORS_STRING = "Scissors";
+
 const ROUNDS = 5;
 
 // Declare player and computer score variables
@@ -45,32 +49,50 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
   // Rock game conditions
   if (humanChoice === ROCK && computerChoice === SCISSORS) {
+    console.log(`You chose ${ROCK_STRING} | Computer chose ${SCISSORS_STRING}`);
+    console.log(`${ROCK_STRING} beats ${SCISSORS_STRING}`);
     humanScore++;
     console.log(`You won the round`);
   } else if (humanChoice === ROCK && computerChoice === PAPER) {
+    console.log(`You chose ${ROCK_STRING} | Computer chose ${PAPER_STRING}`);
+    console.log(`${ROCK_STRING} loses to ${SCISSORS_STRING}`);
     computerScore++;
     console.log(`You lost the round`);
   } else if (humanChoice === ROCK && computerChoice === ROCK) {
+    console.log(`You chose ${ROCK_STRING} | Computer chose ${ROCK}`);
+    console.log(`${ROCK_STRING} ties with ${ROCK}`);
     console.log(`You tied this round`);
   }
   // Scissor game conditions
   else if (humanChoice === PAPER && computerChoice === ROCK) {
+    console.log(`You chose ${PAPER_STRING} | Computer chose ${ROCK_STRING}`);
+    console.log(`${PAPER_STRING} beats ${ROCK_STRING}`);
     humanScore++;
     console.log(`You won the round`);
   } else if (humanChoice === PAPER && computerChoice === SCISSORS) {
+    console.log(`You chose ${PAPER_STRING} | Computer chose ${SCISSORS_STRING}`);
+    console.log(`${PAPER_STRING} loses to ${PAPER_STRING}`);
     computerScore++;
     console.log(`You lost the round`);
   } else if (humanChoice === PAPER && computerChoice === PAPER) {
+    console.log(`You chose ${PAPER_STRING} | Computer chose ${PAPER_STRING}`);
+    console.log(`${PAPER_STRING} ties with ${PAPER_STRING}`);
     console.log(`You tied this round`);
   }
   // Scissor game conditions
   else if (humanChoice === SCISSORS && computerChoice === PAPER) {
+    console.log(`You chose ${SCISSORS_STRING} | Computer chose ${PAPER_STRING}`);
+    console.log(`${SCISSORS_STRING} beats ${PAPER_STRING}`);
     humanScore++;
     console.log(`You won the round`);
   } else if (humanChoice === SCISSORS && computerChoice === ROCK) {
+    console.log(`You chose ${SCISSORS_STRING} | Computer chose ${ROCK_STRING}`);
+    console.log(`${SCISSORS_STRING} loses to ${ROCK_STRING}`);
     computerScore++;
     console.log(`You lost the round`);
   } else if (humanChoice === SCISSORS && computerChoice === SCISSORS) {
+    console.log(`You chose ${SCISSORS_STRING} | Computer chose ${SCISSORS_STRING}`);
+    console.log(`${SCISSORS_STRING} ties with ${SCISSORS_STRING}`);
     console.log(`You tied this round`);
   }
 }
@@ -83,6 +105,7 @@ function playGame() {
     console.log(`Round ${i} of ${ROUNDS}`);
     playRound(humanSelection, computerSelection);
   }
+  console.log(`HUMAN: ${humanScore} | COMPUTER: ${computerScore}`);
   if (humanScore > computerScore) {
     console.log("You won");
   } else if (humanScore < computerScore) {
