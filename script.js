0575ddae75887f439983669ee0eb2002e6cb96de
p4triko <168976850+p4triko@users.noreply.github.com>
2024-05-15 14:14:12 +0300

    Define consant values of  r,p and s. Create function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+const ROCK = 1;
+const PAPER = 2;
+const SCISSORS = 3;
+
+function getComputerChoice(min, max) {
+  let result = Math.floor(Math.random() * max) + min;
+  return result;
+}
+
+console.log(getComputerChoice(ROCK, SCISSORS));
